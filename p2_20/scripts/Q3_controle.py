f434114170c225ea8ddfae2c369287d5c59e2588
mirwox <fmiranda@gmail.com>
2020-06-15 15:34:35 -0300

    Solucao da Q3 aula de reviao

--- p2_20/scripts/Q3_controle.py
+++ p2_20/scripts/Q3_controle.py
@@ -20,6 +20,7 @@ from tf import transformations
 
 x = None
 y = None
+rad_z = 0.0
 
 contador = 0
 pula = 50
@@ -27,6 +28,7 @@ pula = 50
 def recebe_odometria(data):
     global x
     global y
+    global rad_z
     global contador
 
     x = data.pose.pose.position.x
@@ -34,27 +36,79 @@ def recebe_odometria(data):
 
     quat = data.pose.pose.orientation
     lista = [quat.x, quat.y, quat.z, quat.w]
-    angulos = np.degrees(transformations.euler_from_quaternion(lista))    
+    angulos_rad = transformations.euler_from_quaternion(lista)
+    rad_z = angulos_rad[2]
+    angulos = np.degrees(angulos_rad)    
 
     if contador % pula == 0:
         print("Posicao (x,y)  ({:.2f} , {:.2f}) + angulo {:.2f}".format(x, y,angulos[2]))
     contador = contador + 1
 
+v_ang = 0.3
+v_lin = 0.3
+
+
+def go_to(x1, y1, pub):
+    x0 = x
+    y0 = y
+    delta_x = x1 - x0
+    delta_y = y1 - y0
+
+    h = math.sqrt(delta_x**2 + delta_y**2)
+
+    while h > 0.3:      
+        # Rotacao
+        ang_goal = math.atan2(delta_y,delta_x)  
+        ang_atual = rad_z
+        dif_ang = ang_goal - ang_atual
+        delta_t = abs(dif_ang)/v_ang
+        # Twist
+        if dif_ang > 0.0:
+            vel_rot = Twist(Vector3(0,0,0), Vector3(0,0,v_ang))
+        elif dif_ang <=0:
+            vel_rot = Twist(Vector3(0,0,0), Vector3(0,0,-v_ang))    
+        # publish
+        pub.publish(vel_rot)
+        # sleep
+        rospy.sleep(delta_t)
+        zero = Twist(Vector3(0,0,0), Vector3(0,0,0))
+        pub.publish(zero)
+        rospy.sleep(0.1)
+        # Translacao
+        delta_t = h/v_lin
+        linear = Twist(Vector3(v_lin,0,0), Vector3(0,0,0))
+        pub.publish(linear)
+        rospy.sleep(delta_t)
+        pub.publish(zero)
+        rospy.sleep(0.1)  
+        x0 = x
+        y0 = y
+        delta_x = x1 - x0
+        delta_y = y1 - y0
+        h = math.sqrt(delta_x**2 + delta_y**2)        
+
+
+
+
+
+
 if __name__=="__main__":
 
     rospy.init_node("q3")
 
+    # Velocidades
+    zero = Twist(Vector3(0,0,0), Vector3(0,0,0))
+
+    linear = Twist(Vector3(v_lin,0,0), Vector3(0,0,0))
     
-
-
-
 
     velocidade_saida = rospy.Publisher("/cmd_vel", Twist, queue_size = 3 )
 
     ref_odometria = rospy.Subscriber("/odom", Odometry, recebe_odometria)
 
-
+    velocidade_saida.publish(zero)
     rospy.sleep(1.0) # contorna bugs de timing    
 
     while not rospy.is_shutdown():
         rospy.sleep(0.5)    
+        go_to(3,5, velocidade_saida)
